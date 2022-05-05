ea145ebce6a5d39c7c82e675a8f590ac264d91bd
CIARAVELLA Robin <ciarav_r@etna-alternance.net>
2022-05-05 16:53:20 +0200

    duppl zeroreact

---
+++ zeroreact/src/Contact.js
@@ -0,0 +1,7 @@
+const Contact = () => {
+    return (
+        <h2>Hello app</h2>
+    )
+}
+
+export default Contact;
---
+++ zeroreact/src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../component/Logo';
+import Navigation from '../component/Navigation';
+
+const Contact = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut ab, perspiciatis quam porro tenetur cumque accusantium pariatur cum temporibus, 
+                veritatis consectetur impedit totam itaque quod deleniti neque voluptas vitae architecto.</p>
+                <br />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium consequuntur tenetur qui perspiciatis enim? Modi,
+                     quasi veniam veritatis, expedita nobis amet, possimus est delectus quas quod enim nihil incidunt eos!</p>
+        </div>
+    );
+};
+
+export default Contact;
